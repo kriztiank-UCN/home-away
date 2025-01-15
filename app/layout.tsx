2924b00618a9b6cbc8f291f458dbfa477a65c926
Kristian <10221425@ucn.dk>
2025-01-15 09:09:15 +0100

    Setup and Customize Clerk Auth Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Navbar from '@/components/navbar/Navbar';
-import Providers from './providers';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HomeAway Draft',
-  description: 'Feel at home, away from home.',
+  title: "HomeAway Draft",
+  description: "Feel at home, away from home.",
 };
 
 export default function RootLayout({
@@ -17,13 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>
-        <Providers>
-          <Navbar />
-          <main className='container py-10'>{children}</main>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>
+          <Providers>
+            <Navbar />
+            <main className='container py-10'>{children}</main>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
