09c08cdc99fa5c3b6f9353ff4394f1234e6777df
Kristian <10221425@ucn.dk>
2025-01-19 11:27:28 +0100

    Refactor Create Profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,11 +1,14 @@
-// import { Label } from '@/components/ui/label';
-// import { Input } from '@/components/ui/input';
-// import { Button } from "@/components/ui/button";
+import FormInput from "@/components/form/FormInput";
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
   "use server";
   const firstName = formData.get("firstName") as string;
   console.log(firstName);
+  // toast message
+  if (firstName !== "shakeAndBake") return { message: "there was an error..." };
+  return { message: "Profile Created" };
 };
 
 function CreateProfilePage() {
@@ -13,15 +16,14 @@ function CreateProfilePage() {
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
       <div className='border p-8 rounded-md max-w-lg'>
-        <form action={createProfileAction}>
-          {/* <div className='mb-2'>
-            <Label htmlFor='firstName'>First Name</Label>
-            <Input id='firstName' name='firstName' type='text' />
-          </div> */}
-          {/* <Button type='submit' size='lg'>
-            Create Profile
-          </Button> */}
-        </form>
+        <FormContainer action={createProfileAction}>
+          <div className='grid gap-4 mt-4 '>
+            <FormInput type='text' name='firstName' label='First Name' />
+            <FormInput type='text' name='lastName' label='Last Name' />
+            <FormInput type='text' name='username' label='Username' />
+          </div>
+          <SubmitButton text='Create Profile' className='mt-8' />
+        </FormContainer>
       </div>
     </section>
   );
