5223e2661a330d6b77fb0b3d6a7c78a929c29b26
Kristian <10221425@ucn.dk>
2025-02-15 09:10:32 +0100

    Favorites Page

--- utils/actions.ts
+++ utils/actions.ts
@@ -243,3 +243,25 @@ export const toggleFavoriteAction = async (prevState: {
   }
 };
 
+// FETCH FAVORITES ACTION
+export const fetchFavorites = async () => {
+  const user = await getAuthUser();
+  const favorites = await db.favorite.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          tagline: true,
+          price: true,
+          country: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return favorites.map((favorite) => favorite.property);
+};
