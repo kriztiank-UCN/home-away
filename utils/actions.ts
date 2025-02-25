bf0701e17136ea3def1c8030bfda96e973c3b26b
Kristian <10221425@ucn.dk>
2025-02-25 11:19:04 +0100

    Fetch Reviews

--- utils/actions.ts
+++ utils/actions.ts
@@ -277,7 +277,7 @@ export const fetchPropertyDetails = (id: string) => {
   });
 };
 
-// temp placeholder functions
+// CREATE REVIEW ACTION
 export async function createReviewAction(prevState: any, formData: FormData) {
   const user = await getAuthUser();
   try {
@@ -297,9 +297,29 @@ export async function createReviewAction(prevState: any, formData: FormData) {
   }
 }
 
-export const fetchPropertyReviews = async () => {
-  return { message: 'fetch reviews' };
-};
+// FETCH PROPERTY REVIEWS
+export async function fetchPropertyReviews(propertyId: string) {
+  const reviews = await db.review.findMany({
+    where: {
+      propertyId,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      profile: {
+        select: {
+          firstName: true,
+          profileImage: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return reviews;
+}
 
 export const fetchPropertyReviewsByUser = async () => {
   return { message: 'fetch user reviews' };
