625ac787549c518011f5cabbb143351c5d8fc35c
Kristian <10221425@ucn.dk>
2025-03-01 10:40:46 +0100

    Confirm Booking Component

--- components/booking/ConfirmBooking.tsx
+++ components/booking/ConfirmBooking.tsx
@@ -1,6 +1,38 @@
-const ConfirmBooking = () => {
+"use client";
+import { SignInButton, useAuth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { useProperty } from "@/utils/store";
+import FormContainer from "@/components/form/FormContainer";
+import { SubmitButton } from "@/components/form/Buttons";
+import { createBookingAction } from "@/utils/actions";
+
+function ConfirmBooking() {
+  const { userId } = useAuth();
+  const { propertyId, range } = useProperty(state => state);
+  const checkIn = range?.from as Date;
+  const checkOut = range?.to as Date;
+  if (!userId)
+    return (
+      <SignInButton mode='modal'>
+        <Button type='button' className='w-full'>
+          Sign In to Complete Booking
+        </Button>
+      </SignInButton>
+    );
+
+  // Create a booking action which will bind the propertyId, checkIn, and checkOut values
+  const createBooking = createBookingAction.bind(null, {
+    propertyId,
+    checkIn,
+    checkOut,
+  });
+
   return (
-    <div>ConfirmBooking</div>
-  )
+    <section>
+      <FormContainer action={createBooking}>
+        <SubmitButton text='Reserve' className='w-full' />
+      </FormContainer>
+    </section>
+  );
 }
-export default ConfirmBooking
+export default ConfirmBooking;
