e3d8d4f03e132d0921b74d7f7332f6eee6decd3e
Kristian <10221425@ucn.dk>
2025-03-02 11:08:46 +0100

    Disabled Dates

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { Calendar } from "@/components/ui/calendar";
 import { useEffect, useState } from "react";
@@ -18,13 +19,28 @@ function BookingCalendar() {
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
   // Zustand
   const bookings = useProperty(state => state.bookings);
-  
+  const { toast } = useToast();
+  // Blocked Periods/Dates
   const blockedPeriods = generateBlockedPeriods({
     bookings,
     today: currentDate,
   });
+  // Unavailable Dates
+  const unavailableDates = generateDisabledDates(blockedPeriods);
+  console.log(unavailableDates);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    selectedRange.some(date => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked. Please select again.",
+        });
+        return true;
+      }
+      return false;
+    });
     useProperty.setState({ range });
   }, [range]);
 
